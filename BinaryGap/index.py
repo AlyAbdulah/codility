6547eaaca75c0d8dabd10009e47190c2e796672c
AlyAbdullah <accounts@alyabdulah.com>
2023-08-17 11:12:38 +0500

    Completed Binary Gap Problem

--- BinaryGap/index.py
+++ BinaryGap/index.py
@@ -1,7 +1,20 @@
 def solution(N):
+    maxlen = 0
+    # Converting number to binary and removing starting binary code i.e 0b.
     binary = bin(N).replace("0b", "")
+    # converting number to string then to list
     numbers = list(str(binary))
+    foundOne = False
+    counter = 0
     for num in numbers:
-        print(num)
-
-solution(4)
+        if num == str(1):
+            if foundOne == True:
+                if counter > maxlen:
+                    maxlen = counter
+                counter = 0
+            else:
+                foundOne = True
+        else:
+            if foundOne == True:
+                counter = counter + 1
+    return maxlen
