afbc5c2bd017903808bb625b7aa87710f4d15fa3
AlyAbdullah <accounts@alyabdulah.com>
2023-08-22 16:35:42 +0500

    Updated to Prevent Errors

--- CyclicRotation/index.py
+++ CyclicRotation/index.py
@@ -1,7 +1,8 @@
 def solution(A, K):
     NA = A
-    NA.insert(0, A[len(A) - 1])
-    NA.pop()
-    if K > 1:
-        solution(NA, K - 1)
+    if len(A) > 0:
+        NA.insert(0, A[len(A) - 1])
+        NA.pop()
+        if K > 1:
+            solution(NA, K - 1)
     return NA
